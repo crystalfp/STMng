e667538ca3a655274f449d3c1b8d8b7387d7540a
Mario Valle <mvalle@cscs.ch>
2024-01-21 05:46:43 +0100

    Initial attempt to native module

--- src/electron/modules/Symmetries.ts
+++ src/electron/modules/Symmetries.ts
@@ -5,11 +5,22 @@
  */
 import {ipcMain} from "electron";
 
+interface NativeModule {
+	hello: () => void;
+}
+
 export const setupChannelSymmetries = (): void => {
+
+	/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,
+	   @typescript-eslint/no-var-requires, unicorn/prefer-module */
+	const addon = require("bindings")("hello") as NativeModule;
 
 	ipcMain.handle("COMPUTE:SYMMETRIES",  (_event, spaceGroup: string, fractionalCoords: number[]) => {
 
 		// TBD Symmetry computation
+		const data = new Float32Array(fractionalCoords);
+		void data;
+		console.log("Native addon:", addon.hello());
 		console.log("Received:", spaceGroup, "and", fractionalCoords.length);
 		return {payload: JSON.stringify(fractionalCoords)};
 	});
