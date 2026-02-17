95a919d347491f21e3e38acea6b54cfbadcf1d38
Mario Valle <mvalle@ikmail.com>
2026-02-17 11:03:02 +0100

    Updated version. Removed atom duplicates in collection.

--- src/electron/nodes/CollectionMatcher.ts
+++ src/electron/nodes/CollectionMatcher.ts
@@ -14,7 +14,8 @@ import {collectionLoadFingerprints, collectionGetNearestStructures,
 import {publicDirPath} from "../modules/GetPublicPath";
 import {createOrUpdateSecondaryWindow} from "../modules/WindowsUtilities";
 import {getAtomData} from "../modules/AtomData";
-import {hasNoUnitCell, invertBasis} from "../modules/Helpers";
+import {hasNoUnitCell} from "../modules/Helpers";
+import {markDuplicates} from "../modules/MarkDuplicates";
 import type {Atom, Crystal, ChannelDefinition,
 			 CtrlParams, FingerprintingParameters,
 			 PrototypeAtomsData, Structure} from "@/types";
@@ -32,9 +33,9 @@ export class CollectionMatcher extends NodeCore {
 	};
 
 	private readonly channels: ChannelDefinition[] = [
-		{name: "init",		type: "invoke", callback: this.channelInit.bind(this)},
-		{name: "state",		type: "send",	callback: this.channelState.bind(this)},
-		{name: "show",		type: "invoke", callback: this.channelShow.bind(this)},
+		{name: "init",	type: "invoke", callback: this.channelInit.bind(this)},
+		{name: "state",	type: "send",	callback: this.channelState.bind(this)},
+		{name: "show",	type: "invoke", callback: this.channelShow.bind(this)},
 	];
 
 	/**
@@ -133,112 +134,6 @@ export class CollectionMatcher extends NodeCore {
 	}
 
 	/**
-	 * Mark atoms of the input structure that should be ignored because
-	 * are duplicates or outside the unit cell
-	 *
-	 * @param atoms - Input structure atoms
-	 * @param crystal - Input structure crystal data
-	 * @returns The excluded markers (one per atom)
-	 */
-	private markDuplicates(atoms: Atom[], crystal: Crystal): boolean[] {
-
-		const {basis, origin} = crystal;
-		const natoms = atoms.length;
-		const TOL = 1e-6;
-
-		const inverse = invertBasis(basis);
-		const frac: number[][] = [];
-		const excluded = Array<boolean>(natoms).fill(false);
-
-		for(let i=0; i < natoms; ++i) {
-
-			const {position} = atoms[i];
-
-			const cx = position[0] - origin[0];
-			const cy = position[1] - origin[1];
-			const cz = position[2] - origin[2];
-
-			const fx = cx*inverse[0] + cy*inverse[3] + cz*inverse[6];
-			const fy = cx*inverse[1] + cy*inverse[4] + cz*inverse[7];
-			const fz = cx*inverse[2] + cy*inverse[5] + cz*inverse[8];
-
-			if(fx < 0 || fx > 1 || fy < 0 || fy > 1 || fz < 0 || fz > 1) {
-				excluded[i] = true;
-			}
-
-			frac.push([fx, fy, fz]);
-		}
-
-		// Remove atoms on the opposite sides of the cell
-		for(let i=0; i < natoms-1; ++i) {
-
-			if(excluded[i]) continue;
-
-			const li =[
-				frac[i][0] < TOL || frac[i][0] > 1-TOL,
-				frac[i][1] < TOL || frac[i][1] > 1-TOL,
-				frac[i][2] < TOL || frac[i][2] > 1-TOL,
-			];
-			const liAny = li[0] || li[1] || li[2];
-
-			for(let j=i+1; j < natoms; ++j) {
-
-				if(excluded[j]) continue;
-
-				const dx = Math.abs(frac[i][0] - frac[j][0]);
-				const dy = Math.abs(frac[i][1] - frac[j][1]);
-				const dz = Math.abs(frac[i][2] - frac[j][2]);
-
-				const ld = [
-					dx < TOL,
-					dy < TOL,
-					dz < TOL
-				];
-				if(ld[0] && ld[1] && ld[2]) excluded[j] = true;
-				else if(!liAny) continue;
-
-				const lj =[
-					frac[j][0] < TOL || frac[j][0] > 1-TOL,
-					frac[j][1] < TOL || frac[j][1] > 1-TOL,
-					frac[j][2] < TOL || frac[j][2] > 1-TOL,
-				];
-				const ljAny = lj[0] || lj[1] || lj[2];
- 				if(!ljAny) continue;
-
-				excluded[j] = this.checkCorrespondence(li, lj, ld);
-			}
-		}
-
-		return excluded;
-	}
-
-	/**
-	 * Check if two atoms are in reality the same atom
-	 *
-	 * @param li - If atom "i" is on the border (for each axis)
-	 * @param lj - If atom "j" is on the border (for each axis)
-	 * @param ld - If coordinates coincide (for each axis)
-	 * @returns True if the two atoms are crystallographic correspondents
-	 */
-	private checkCorrespondence(li: boolean[], lj: boolean[], ld: boolean[]): boolean {
-
-		// On the corners
-		if(li[0] && li[1] && li[2] && lj[0] && lj[1] && lj[2]) return true;
-
-		// On the edges
-		if(li[0] && li[1] && lj[0] && lj[1] && ld[2]) return true;
-		if(li[0] && li[2] && lj[0] && lj[2] && ld[1]) return true;
-		if(li[1] && li[2] && lj[1] && lj[2] && ld[0]) return true;
-
-		// On the facies
-		if(li[0] && lj[0] && ld[1] && ld[2]) return true;
-		if(li[1] && lj[1] && ld[0] && ld[2]) return true;
-		if(li[2] && lj[2] && ld[0] && ld[1]) return true;
-
-		return false;
-	}
-
-	/**
 	 * Find similar structures from the collection
 	 */
 	private findSimilar(): void {
@@ -259,7 +154,7 @@ export class CollectionMatcher extends NodeCore {
 		const {atoms, crystal} = this.structure;
 
 		// Remove duplicate and outside unit cell atoms
-		const duplicates = this.markDuplicates(atoms, crystal);
+		const duplicates = markDuplicates(atoms, crystal);
 
 		// Compute the input structure fingerprint
 		const fp = this.computeFingerprint(atoms, crystal, duplicates);
