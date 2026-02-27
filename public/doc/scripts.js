8dbe74d04b0c0642eeb74ef835bba2a2be266759
Mario Valle <mvalle@ikmail.com>
2026-02-27 15:42:31 +0100

    Initial load

---
+++ public/doc/scripts.js
@@ -0,0 +1,21 @@
+let areOpen = false;
+const toggleDetails = () => {
+	if(areOpen) {
+		document.body.querySelectorAll('details')
+			.forEach((e) => {e.removeAttribute('open')})
+		areOpen = false
+	}
+	else {
+		document.body.querySelectorAll('details')
+			.forEach((e) => {e.setAttribute('open',true);})
+		areOpen = true
+	}
+}
+const openDetails = (anchor) => {
+	const element = document.querySelector(anchor); console.log(element);
+	element.setAttribute('open',true);
+	element.scrollIntoView();
+}
+const openMain = (url) => {
+	window.open(url, "_self");
+}
